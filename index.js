afd74fa7b932c8343ba72217bea22df350c16188
Kisha Prudente <kisha.prudente@gmail.com>
2020-04-21 23:54:33 -0400

    added questions array

--- index.js
+++ index.js
@@ -1,12 +1,58 @@
+require("dotenv").config();
+const inquirer = require("inquirer");
+const fs = require("fs");
+const axios = require("axios");
+const api = require("./utils/api");
+
 const questions = [
-
+  {
+    type: "input",
+    name: "username",
+    message: "What is your GitHub username?",
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "What is your Project title?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Describe your project",
+  },
+  {
+    type: "checkbox",
+    name: "tableOfContents",
+    message: "What should be in your Table of Contents?",
+    choices: [
+      "Project Title",
+      "Description",
+      "Installation",
+      "Usage",
+      "Lisense",
+      "Contributing",
+      "Tests",
+      "Questions",
+    ],
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "How do we install?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "How do we use the project?",
+  },
+  {
+    type: "input",
+    name: "contributors",
+    message: "Who are the contributors?",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "How do we test?",
+  },
 ];
-
-function writeToFile(fileName, data) {
-}
-
-function init() {
-
-}
-
-init();
