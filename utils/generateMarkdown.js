ba10440782dfa234698fd0ddfbb61e2369c2be88
Kisha Prudente <kisha.prudente@gmail.com>
2020-04-22 21:31:50 -0400

    added function to generatedMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,34 @@ function generateMarkdown(data) {
   return `
 # ${data.title}
 
+### ${data.description}
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributors](#contributors)
+* [Tests](#test)
+
+## Installation
+    ${data.installation}
+
+## Usage
+#### run in the terminal
+    ${data.usage}
+
+## Tests
+    ${data.test}
+
+## Contributing
+${data.contributing}
+
+### Questions?
+![Bio Image](${data.avatar}&s=50)
+
+#### Email: ${data.email}
+
 `;
 }
 
